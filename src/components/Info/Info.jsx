bd45b7aa9043b03c3ccd5687a5deea4b4cba445f
Benjamin Chipinde <benjamin@kylix.online>
2022-06-24 11:54:58 +0300

    info setup

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Info.css'
 
 function Info() {
   return (
-    <div>Info Section</div>
+    <div>
+      <img src="" alt="DP"></img>
+      <h2>Benjamin Rau Chipinde</h2>
+      <h3>Nodejs Developer</h3>
+      <a href="https://rau-portfolio.herokuapp.com/">rau.portfolio</a>
+      <br></br>
+      <button type="button">Email</button>
+    </div>
   )
 }
 
